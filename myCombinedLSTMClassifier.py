9094a8f153ab0d28dff9db506bef82df7f082416
Anıl Ergan <ergananil@gmail.com>
2023-09-13 19:31:21 +0300

    commit

--- myCombinedLSTMClassifier.py
+++ myCombinedLSTMClassifier.py
@@ -1,4 +1,5 @@
 from keras.models import Sequential as sq
+from tensorflow import float32, convert_to_tensor
 from keras.layers import Dense
 from keras.layers import LSTM
 from keras.layers import Dropout
@@ -10,14 +11,15 @@ from time import time
 
 class My_Combined_LSTM_Classifier():
 
-    def __init__(self, x, y, test_num=200, time_steps=60,  hidden_layer_num = 3, units=50, dropout=0.2, epoch=50, batch_size=32, model_activation = 'relu', predict=True, fit_verbose = 1):
-        self.X = x
+    def __init__(self, x, y, test_num, time_steps=10,  hidden_layer_num = 3, units=50, dropout=0.2, epoch=10, batch_size=32, model_activation = 'relu', predict=True, fit_verbose = 1):
+        self.X = x[:, :-1]
+        self.X_change = x[:, -1:]
         self.Y = y
 
         self.fit_verbose = fit_verbose
         self.time_steps = time_steps 
         
-        self.x_train, self.x_test, self.y_train, self.y_test = self.train_test_split(test_num)
+        self.x_train, self.x_test, self.x_change_test, self.y_train, self.y_test = self.train_test_split(test_num)
         
         self.X_train, self.Y_train = self.train_set_ts_split()
 
@@ -28,9 +30,10 @@ class My_Combined_LSTM_Classifier():
         self.run(epoch, batch_size)
 
         if predict:
-            accuracy, con_matrix = self.evaluate()
-            print('Model Accuracy: ', accuracy)
-            print('\nConfussion Matrix:\n', con_matrix)
+            test_accuracy, train_accuracy, profit = self.evaluate()
+            print('Train Accuracy: ', train_accuracy)
+            print('Test Accuracy:', test_accuracy)
+            print(f'Profit: {profit}%')
 
             # self.y_test_inv = self.scaler_model.inverse_transform(self.y_test)
             # self.y_pred_inv = self.scaler_model.inverse_transform(self.y_pred)
@@ -39,12 +42,15 @@ class My_Combined_LSTM_Classifier():
         
     def train_test_split(self, test_num):
         train_num = self.X.shape[0] - test_num
+
         x_train = self.X[:train_num]
         x_test = self.X[train_num:]
 
+        x_change_test = self.X_change[train_num:]
+
         y_train = self.Y[:train_num]
         y_test = self.Y[train_num:]
-        return x_train, x_test, y_train, y_test
+        return x_train, x_test, x_change_test, y_train, y_test
 
 
     def train_set_ts_split(self):
@@ -100,24 +106,65 @@ class My_Combined_LSTM_Classifier():
 
         
         # fit model
-        self.model.fit(self.X_train, self.Y_train, epochs = epoch, batch_size = batch_size, verbose=self.fit_verbose)
+        self.X_train = convert_to_tensor(self.X_train, dtype=float32)
+        self.Y_train = convert_to_tensor(self.Y_train, dtype=float32)
+
+        self.model_history = self.model.fit(self.X_train, self.Y_train, epochs = epoch, batch_size = batch_size, verbose=self.fit_verbose)
 
 
     def evaluate(self):
-        y_pred = self.model.predict(self.X_test, verbose=self.fit_verbose)
-
-        # Düz tahminleri kategorik etiketlere dönüştür
-        y_pred_category = np.argmax(y_pred, axis=1)
+        self.X_test = convert_to_tensor(self.X_test, dtype=float32)
+        self.y_pred = self.model.predict(self.X_test, verbose=self.fit_verbose)
+        # # Düz tahminleri kategorik etiketlere dönüştür
+        y_pred_category = np.argmax(self.y_pred, axis=1)
         y_test_category = np.argmax(self.y_test, axis=1)
 
         # Accuracy hesapla
-        accuracy = accuracy_score(y_test_category, y_pred_category)
-        formatted_accuracy = round(accuracy * 100, 2)
+        test_accuracy = accuracy_score(y_test_category, y_pred_category)
+        test_accuracy = round(test_accuracy * 100, 2)
 
-        con_matrix = confusion_matrix(y_test_category, y_pred_category)
+        train_accuracy = self.model_history.history['accuracy'][-1]
+        train_accuracy = round(train_accuracy * 100, 2) 
 
-        return formatted_accuracy, con_matrix
+        investment_profit = self.test_investment(y_pred_category)
 
+        # con_matrix = confusion_matrix(y_test_category, y_pred_category)
+
+        return test_accuracy, train_accuracy, investment_profit
+
+
+    def test_investment(self, y_pred):
+        pred_df = pd.DataFrame(data = 0, index=range(len(y_pred)), columns=['bear','volatile', 'bull', 'change', 'investment'])
+
+        for i in range(len(y_pred)):
+            if y_pred[i] == 0:
+                pred_df.iloc[i, 0] = 1
+            elif y_pred[i] == 1: 
+                pred_df.iloc[i, 1] = 1
+            elif y_pred[i] == 2:
+                pred_df.iloc[i, 2] = 1
+        for i in range(len(self.x_change_test)):
+            pred_df.iloc[i, 3] = self.x_change_test[i]
+
+        
+        pred_df.iloc[:, 4] = 100
+        
+        for i in range(0, len(pred_df)-1):
+            if pred_df.loc[i, 'bull'] == 1:
+                pred_df.loc[i+1, 'investment'] = (pred_df.loc[i, 'investment'] * pred_df.loc[i+1, 'change'] / 100) + pred_df.loc[i-1, 'investment'] #inside
+            elif pred_df.loc[i, 'volatile'] == 1: pred_df.loc[i+1, 'investment'] = pred_df.loc[i, 'investment'] #not insid
+        
+            elif pred_df.loc[i, 'bear'] == 1: pred_df.loc[i+1, 'investment'] = pred_df.loc[i, 'investment'] #not inside
+
+        pred_df.loc[:,['bear', 'volatile', 'bull', 'change', 'investment']]
+
+        investment_profit = round(pred_df.iloc[-1, -1] - pred_df.iloc[0, -1], 2)
+
+
+        return investment_profit
+
+        
+        
 
 
     def model_tune(self, test_num, epoch, time_steps, units, dropout, hidden_layer_num, model_activation, batch_size):
@@ -127,6 +174,7 @@ class My_Combined_LSTM_Classifier():
         model_tune_array = np.array([])
 
         print('THE MODEL IS TUNING!\nSEE U AN ETERNITY LATER *_*\n')
+        print(f'Total Progress is creating by {total_process} steps.\n')
         for tn in test_num:
             for e in epoch:
                 for ts in time_steps:
@@ -140,7 +188,7 @@ class My_Combined_LSTM_Classifier():
 
                                             self.time_steps = ts
 
-                                            self.x_train, self.x_test, self.y_train, self.y_test = self.train_test_split(tn)
+                                            self.x_train, self.x_test, self.x_change_test, self.y_train, self.y_test = self.train_test_split(tn)
                                             
                                             self.X_train, self.Y_train = self.train_set_ts_split()
 
@@ -150,12 +198,12 @@ class My_Combined_LSTM_Classifier():
 
                                             self.run(e, bs)
 
-                                            accuracy = self.evaluate()
-                                            accuracy = np.array([accuracy])
+                                            test_acc, train_acc, profit = self.evaluate()
+                                            results = np.array([profit, test_acc, train_acc])
 
                                             variables = np.array([tn, e, ts, u, d, hln, ma, bs])
                                             
-                                            model_tune_array = np.append(model_tune_array, np.concatenate([accuracy, variables]))
+                                            model_tune_array = np.append(model_tune_array, np.concatenate([results, variables]))
 
                                             end_time = time()
                                 
@@ -177,17 +225,17 @@ class My_Combined_LSTM_Classifier():
                                             process_step += 1
 
         
-        model_tune_array = model_tune_array.reshape(total_process,9)
+        model_tune_array = model_tune_array.reshape(total_process,model_tune_array.shape[1])
 
         def model_tune_exhibit(table):
-            table_df = pd.DataFrame(data=table, columns=['accuracy', 'test_num', 'epoch', 'time_steps', 'units', 'dropout', 'hidden_layer_num', 'model_activation', 'batch_size'])
+            table_df = pd.DataFrame(data=table, columns=['profit(%)', 'test_accuracy(%)', 'train_accuracy(%)', 'test_num', 'epoch', 'time_steps', 'units', 'dropout', 'hidden_layer_num', 'model_activation', 'batch_size'])
 
             table_df['index'] = table_df.index
             table_df = table_df[table_df.columns[-1:].to_list() + table_df.columns[:-1].to_list()]
             
             pd.set_option('display.width', 500)
 
-            print('\n', '-|'*17, ' Top 10 Accuracy and Variables ','-|'*17)
-            print(table_df.sort_values(by= 'accuracy', ascending=False).iloc[:10, :])
+            print('\n', '-|'*17, ' Top 10 Profit and Variables ','-|'*17)
+            print(table_df.sort_values(by= 'profit(%)', ascending=False).iloc[:10, :])
              
         model_tune_exhibit(model_tune_array)
